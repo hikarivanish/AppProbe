a76b36b626fd1386c192000ea70b30eb17c51f05
hikarivanish <hikarivanish@foxmail.com>
2015-10-09 21:34:01 +0800

    Initial commit

---
+++ app/src/main/java/me/s4h/appprobe/MainActivity.java
@@ -0,0 +1,32 @@
+package me.s4h.appprobe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.myBtn)
+    Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.myBtn)
+    public void onBtnClicked() {
+        Toast.makeText(this, "helloworld", Toast.LENGTH_SHORT).show();
+    }
+
+}
