90b645902fd862c97014b39ad03e6d0f7ea001a6
hikarivanish <hikarivanish@foxmail.com>
2015-10-11 00:52:32 +0800

    得到性能数据

--- app/src/main/java/me/s4h/appprobe/MainActivity.java
+++ app/src/main/java/me/s4h/appprobe/MainActivity.java
@@ -1,25 +1,26 @@
 package me.s4h.appprobe;
 
-import android.graphics.Color;
-import android.graphics.Typeface;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Button;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.*;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,11 +28,17 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.myBtn)
-    Button btn;
 
-    @Bind(R.id.chart)
-    LineChart mChart;
+    @Bind(R.id.processChooser)
+    Spinner mProcessChooser;
+    @Bind(R.id.tv)
+    TextView tv;
+
+    @Bind(R.id.intervalChooser)
+    Spinner mIntervalChooser;
+
+    ProcessChooserAdapter pcAdapter;
+    ActivityManager mActivityManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,182 +46,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-
-        // no description text
-        mChart.setDescription("");
-        mChart.setNoDataTextDescription("You need to provide data for the chart.");
-
-        // enable value highlighting
-        mChart.setHighlightEnabled(true);
-
-        // enable touch gestures
-        mChart.setTouchEnabled(true);
-
-        // enable scaling and dragging
-        mChart.setDragEnabled(true);
-        mChart.setScaleEnabled(true);
-        mChart.setDrawGridBackground(false);
-
-        // if disabled, scaling can be done on x- and y-axis separately
-        mChart.setPinchZoom(true);
-
-        // set an alternative background color
-        mChart.setBackgroundColor(Color.LTGRAY);
-
-        LineData data = new LineData();
-        data.setValueTextColor(Color.WHITE);
-
-        // add empty data
-        mChart.setData(data);
-
-//        Typeface tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
-
-        // get the legend (only possible after setting data)
-        Legend l = mChart.getLegend();
-
-        // modify the legend ...
-        // l.setPosition(LegendPosition.LEFT_OF_CHART);
-        l.setForm(Legend.LegendForm.LINE);
-//        l.setTypeface(tf);
-        l.setTextColor(Color.WHITE);
-
-        XAxis xl = mChart.getXAxis();
-//        xl.setTypeface(tf);
-        xl.setTextColor(Color.WHITE);
-        xl.setDrawGridLines(false);
-        xl.setAvoidFirstLastClipping(true);
-        xl.setSpaceBetweenLabels(5);
-        xl.setEnabled(true);
-
-        YAxis leftAxis = mChart.getAxisLeft();
-//        leftAxis.setTypeface(tf);
-        leftAxis.setTextColor(Color.WHITE);
-        leftAxis.setAxisMaxValue(100f);
-        leftAxis.setAxisMinValue(0f);
-        leftAxis.setStartAtZero(false);
-        leftAxis.setDrawGridLines(true);
-
-        YAxis rightAxis = mChart.getAxisRight();
-        rightAxis.setEnabled(false);
-
+        pcAdapter = new ProcessChooserAdapter(this);
+        mProcessChooser.setAdapter(pcAdapter);
     }
 
-    private int year = 2015;
-    protected String[] mMonths = new String[]{
-            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"
-    };
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mActivityManager =
+                (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+        this.pcAdapter.clear();
+        this.pcAdapter.addAll(mActivityManager.getRunningAppProcesses());
 
-    private void addEntry() {
-
-        LineData data = mChart.getData();
-
-        if (data != null) {
-
-            LineDataSet set = data.getDataSetByIndex(0);
-            // set.addEntry(...); // can be called as well
-
-            if (set == null) {
-                set = createSet();
-                data.addDataSet(set);
-            }
-
-            // add a new x-value first
-            data.addXValue(mMonths[data.getXValCount() % 12] + " "
-                    + (year + data.getXValCount() / 12));
-            data.addEntry(new Entry((float) (Math.random() * 40) + 30f, set.getEntryCount()), 0);
-
-
-            // let the chart know it's data has changed
-            mChart.notifyDataSetChanged();
-
-            // limit the number of visible entries
-            mChart.setVisibleXRangeMaximum(120);
-            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
-
-            // move to the latest entry
-            mChart.moveViewToX(data.getXValCount() - 121);
-
-            // this automatically refreshes the chart (calls invalidate())
-            // mChart.moveViewTo(data.getXValCount()-7, 55f,
-            // AxisDependency.LEFT);
-        }
-    }
-
-    private LineDataSet createSet() {
-
-        LineDataSet set = new LineDataSet(null, "Dynamic Data");
-        set.setAxisDependency(YAxis.AxisDependency.LEFT);
-        set.setColor(ColorTemplate.getHoloBlue());
-        set.setCircleColor(Color.WHITE);
-        set.setLineWidth(2f);
-        set.setCircleSize(4f);
-        set.setFillAlpha(65);
-        set.setFillColor(ColorTemplate.getHoloBlue());
-        set.setHighLightColor(Color.rgb(244, 117, 117));
-        set.setValueTextColor(Color.WHITE);
-        set.setValueTextSize(9f);
-        set.setDrawValues(false);
-        return set;
-    }
-
-    private void feedMultiple() {
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                for (int i = 0; i < 500; i++) {
-
-                    runOnUiThread(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            addEntry();
-                        }
-                    });
-
-                    try {
-                        Thread.sleep(35);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
     }
 
     @OnClick(R.id.myBtn)
     public void onBtnClicked() {
         Toast.makeText(this, "helloworld", Toast.LENGTH_SHORT).show();
-//        new MyTask().execute();
-        feedMultiple();
+        RunningAppProcessInfo info = (RunningAppProcessInfo) mProcessChooser.getSelectedItem();
+        new MyTask().execute(info.processName);
     }
 
 
-    private class MyTask extends AsyncTask<Void, int[], Void> {
+    private class MyTask extends AsyncTask<String, String, Void> {
+        java.lang.Process p = null;
+        BufferedReader br = null;
 
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-
-        @Override
-        protected void onProgressUpdate(int[]... values) {
+        protected void onProgressUpdate(String... values) {
+            MainActivity.this.tv.setText(values[0]);
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
-            while (true) {
-                int[] info = MainActivity.getCpuUsageStatistic();
-                this.publishProgress(info);
+        protected Void doInBackground(String... params) {
+            try {
+                p = Runtime.getRuntime().exec("top");
+                br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                for (String line = br.readLine(); line != null; line = br.readLine()) {
+                    if(line.contains(params[0])){
+                        Log.i("fdsff", "f43f------->" + line);
+                        publishProgress(line);
+                    }
+                }
+            } catch (IOException e) {
+                Log.e("executeTop", "error in getting first line of top");
+                e.printStackTrace();
+            } finally {
                 try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
+                    br.close();
+                    p.destroy();
+                } catch (IOException e) {
+                    Log.e("executeTop",
+                            "error in closing and destroying top process");
                     e.printStackTrace();
                 }
             }
+            return null;
         }
 
         @Override
@@ -223,58 +110,34 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private static class ProcessChooserAdapter extends ArrayAdapter<RunningAppProcessInfo> {
+        private LayoutInflater inflater;
 
-    /**
-     * @return integer Array with 4 elements: user, system, idle and other cpu
-     * usage in percentage.
-     */
-    private static int[] getCpuUsageStatistic() {
-
-        String tempString = executeTop();
-
-        tempString = tempString.replaceAll(",", "");
-        tempString = tempString.replaceAll("User", "");
-        tempString = tempString.replaceAll("System", "");
-        tempString = tempString.replaceAll("IOW", "");
-        tempString = tempString.replaceAll("IRQ", "");
-        tempString = tempString.replaceAll("%", "");
-        for (int i = 0; i < 10; i++) {
-            tempString = tempString.replaceAll("  ", " ");
+        public ProcessChooserAdapter(Context context) {
+            super(context, 0);
+            inflater = LayoutInflater.from(context);
         }
-        tempString = tempString.trim();
-        String[] myString = tempString.split(" ");
-        int[] cpuUsageAsInt = new int[myString.length];
-        for (int i = 0; i < myString.length; i++) {
-            myString[i] = myString[i].trim();
-            cpuUsageAsInt[i] = Integer.parseInt(myString[i]);
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            TextView text;
+            if (convertView == null) {
+                view = inflater.inflate(android.R.layout.simple_spinner_dropdown_item, parent, false);
+            } else {
+                view = convertView;
+            }
+            text = (TextView) view.findViewById(android.R.id.text1);
+            RunningAppProcessInfo item = getItem(position);
+            text.setText(item.pid + ":" + item.processName);
+            return view;
         }
-        return cpuUsageAsInt;
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return this.getView(position, convertView, parent);
+        }
     }
 
-    private static String executeTop() {
-        java.lang.Process p = null;
-        BufferedReader in = null;
-        String returnString = null;
-        try {
-            p = Runtime.getRuntime().exec("top -n 1");
-            in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while (returnString == null || returnString.contentEquals("")) {
-                returnString = in.readLine();
-                Log.i("fdsff", "f43f------->" + returnString);
-            }
-        } catch (IOException e) {
-            Log.e("executeTop", "error in getting first line of top");
-            e.printStackTrace();
-        } finally {
-            try {
-                in.close();
-                p.destroy();
-            } catch (IOException e) {
-                Log.e("executeTop",
-                        "error in closing and destroying top process");
-                e.printStackTrace();
-            }
-        }
-        return returnString;
-    }
+
 }
